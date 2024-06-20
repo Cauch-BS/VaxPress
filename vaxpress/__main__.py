1b553bb96886dd23bdc3f884627281419b67050a
Cauch-BS <cauchybs@snu.ac.kr>
2024-06-20 16:04:25 +0900

    Modified argparse to support Crossover

--- vaxpress/__main__.py
+++ vaxpress/__main__.py
@@ -226,7 +226,6 @@ def parse_options(scoring_funcs, preset, default_off):
                           '(default: vienna)')
     grp.add_argument('--default-off', default=False, action='store_true',
                      help='turn all fitness functions off by default')
-
     grp = parser.add_argument_group('Optimization Options')
     grp.add_argument('--random-initialization', action='store_true',
                      default=False, help='randomize all codons at the beginning')
@@ -257,6 +256,18 @@ def parse_options(scoring_funcs, preset, default_off):
     grp.add_argument('--winddown-rate', type=float, default=0.9, metavar='RATE',
                      help='mutation rate multiplier when mutation stabilization '
                           'is triggered (default: 0.9)')
+    grp.add_argument('--has-crossover', type = bool, default=False, metavar = 'BOOL',
+                     help='use crossover when generating mutant sequences'
+                     '(default: False)')
+    grp.add_argument('--frequency-crossover', type = float, default=0.5, metavar='FREQ',
+                     help='frequency of crossover when generating mutant sequences '
+                          '(default: 0.5)')
+    grp.add_argument('--crossover-prob', type = float, default = 0.25, METAVAR = 'PROB',
+                     help = 'probability of bases being swapped when crossovre occurs'
+                     '(must not exceed 0.5) (default: 0.25)')
+    grp.add_argument('--crossover-method', type = str , default = 'single', metavar = 'METHOD',
+                     help = 'method of crossover to be used (default: single point crossover)'
+                     '(options: single, double, uniform)')
     grp.add_argument('--species', default='human', metavar='NAME',
                      help='target species (default: human)')
     grp.add_argument('--codon-table', default='standard', metavar='NAME',
