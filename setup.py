bc42ebbd6d34203b55137dbda440d5c0eae9aff5
Chaebeom Sheen <cauchybsheen@gmail.com>
2024-07-05 00:00:41 +0900

    Fixed wrong directory name. Fixed setup version.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 ext_modules = [
     Pybind11Extension(
         'repeats',
-        ["src/utils/remote_repeats.cpp"]
+        ["vaxpress/utils/remote_repeats.cpp"]
     )
 ]
 
