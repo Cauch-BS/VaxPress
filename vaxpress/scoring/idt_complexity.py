40ff67505c03c864d34c91338160c67a4fd02732
Chaebeom Sheen <cauchybsheen@gmail.com>
2024-07-16 00:18:39 +0900

    Allowed Asynchronous Synching for IDT Complexity Calculation

--- vaxpress/scoring/idt_complexity.py
+++ vaxpress/scoring/idt_complexity.py
@@ -24,16 +24,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from . import ScoringFunction
-from ..sequence import Sequence
-from requests import post
-from requests import HTTPBasicAuth
+import os
 import asyncio
 import aiohttp
+from time import time
+from requests import post
+from requests.auth import HTTPBasicAuth
+from typing import Dict, List
+from . import ScoringFunction
+from ..sequence import Sequence
 
-import os
+TOKEN_GEN_TIME = 0
+TOKEN = ''
 
 class IDTComplexity(ScoringFunction):
+    '''Scoring function for complexity score from IDT.'''
     name = 'idt_complexity'
     description = 'Complexity Score from IDT'
 
@@ -47,23 +52,25 @@ class IDTComplexity(ScoringFunction):
     
     _TOKEN_URL = "https://sg.idtdna.com/Identityserver/connect/token"
     _SCORE_URL = "https://sg.idtdna.com/restapi/v1/Complexities/ScreenGblockSequences"
-    _TIMEOUT = 120 # Number of seconds to wait for score query requests to complete
+    _TIMEOUT = 300 # Number of seconds to wait for score query requests to complete
     _MAX_LEN = 3000 # Maximum length of sequences to be considered by the IDT API
     _MIN_LEN = 125 # Minimum length of sequences to be considered by the IDT API
-    _MAX_REQ = 99 # Maximum number of sequences that can be accessed at once
+    _MAX_SEQ = 99 # Maximum number of sequences that can be accessed at once
+    _MAX_REQ = 500 # Maximum number of requests that can be sent to the API server. 
+    _TOKEN_LIFE = 3200 # Number of seconds before the token expires
 
-    def __init__(self, weight, min_length, _length_cds):
+    def __init__(self, weight, min_length, _length_cds) -> None:
         self.weight = weight 
         self.min_length = min_length
         self.username = os.environ.get('IDT_USER', None)
         self.password = os.environ.get('IDT_PASSWD', None)
-        self.client_id = os.environ.get('IDT_CLIENT', 'None')
+        self.client_id = os.environ.get('IDT_CLIENT', None)
         self.client_secret = os.environ.get('IDT_API', None)
         self.priority = 37
-        self.token = self._get_access_token()
+        global TOKEN_GEN_TIME, TOKEN
+        self.token = self._get_access_token() if int(time()) - TOKEN_GEN_TIME > self._TOKEN_LIFE else TOKEN
     
-    
-    def _get_idt_access_token(self) -> str:
+    def _get_access_token(self) -> str:
         """Get access token for IDT API (see: https://www.idtdna.com/pages/tools/apidoc)
         :return: access token string
         """
@@ -71,83 +78,92 @@ class IDTComplexity(ScoringFunction):
             'grant_type': 'password', 
             'username': self.username, 
             'password': self.password, 
-            'scope': 'test'}
+            'scope': 'test'
+            }
         auth = HTTPBasicAuth(self.client_id, self.client_secret)
         result = post(
             self._TOKEN_URL, data, auth=auth, timeout=self._TIMEOUT
         )
 
         if 'access_token' in result.json():
+            global TOKEN_GEN_TIME, TOKEN
+            TOKEN_GEN_TIME = int(time())
+            TOKEN = result.json()['access_token']
             return result.json()['access_token']
-        else:
-            raise Exception('''Access token for IDT API could not be generated. 
-                             Check your credentials.
-                             Make sure you exported your credentials as environmental variables:
-                                IDT_USER = {your IDT username}
-                                IDT_PASSWD = {your IDT password}
-                                IDT_CLIENT = {your IDT client ID}
-                                IDT_API = {your IDT API client password}''')
+        raise ValueError("Error in response from IDT API. Please check credentials.")
     
     @staticmethod
-    def parse_score(json_resp):
+    def parse_score(json_resp: List[List[Dict]]) -> List[int]:
+        """Parse the JSON response from the IDT API to get the complexity score.
+        :param json_resp: JSON response from IDT API"""
         scores = []
         for entries in json_resp:
             score = 0
             for entry in entries:
                 if entry['IsViolated']:
                     score -= entry['Score']
+                elif entry['Message'] == 'Authorization has been denied for this request.':
+                    raise ValueError("Authorization has been denied for this request. Please check credentials.")
             scores.append(score)
         return scores
     
-    async def _send_request(self, session, block):
+    async def _send_request(self, session, block: List[Dict]) -> List[Dict]:
         "Helper function to send an asynchronous API request to IDT."
         async with session.post(self._SCORE_URL, json =  block) as resp:
             resp_list = await resp.json()
-            if len(resp_list) != len(block):
-                raise Exception("Error in response from IDT API. Please try again.")
             return resp_list
-
-    async def score(self, seqs):
+    
+    async def scoring(self, seqs: List[str]) -> List[int]:
         """Calculate the complexity score for a given sequence from the IDT API.
         This system uses the gBlock API, which is intended for sequences between 125 and 3000bp in length.
         Sequences not within this range will throw an exception.
         A complexity score betwee 0 and 10 is synthesizable, while a score greater or equal to 10 is not synthesizable.
         The maximum number of sequences that can be accessed every minute is 500.
         If your population is greater than 500, an error will be thrown."""
-        penalties = []
         queries = []
-        checked_length=False
+        checked_length= False
 
         # Check if the sequence length is within the acceptable range
         for i, seq in enumerate(seqs):
             seq = seq[:- Sequence(seq).get_polyA()]
             if not checked_length:
                 if len(seq) not in range(self._MIN_LEN, self._MAX_LEN):
-                    raise Exception(f"Sequence length must be between {self._MIN_LEN} and {self._MAX_LEN}")
+                    raise ValueError(f"Sequence length must be between {self._MIN_LEN} and {self._MAX_LEN}")
                 checked_length = True
             #set up a list of queries as dictionaries
             queries.append(
-                {"Name": f"seq {i}", "Sequece": seq}
+                {"Name": f"seq_{i}", "Sequence": seq}
             )
         
         # Split the queries into blocks of sequences for more efficient requests. 
         block_query = [
-            queries[i:i + self._MAX_REQ] for i in range(0, len(queries), self._MAX_REQ)
+            queries[i:i + self._MAX_SEQ] for i in range(0, len(queries), self._MAX_REQ)
         ]
-        limit = 500 // self._MAX_REQ # Maximum number of sequences that can be accessed every minute is 500.
+        limit = self._MAX_REQ # Maximum number of requests that can be done every minute is 500.
 
         async with aiohttp.ClientSession(
             headers = {"Authorization": f"Bearer {self.token}", 
                        "Content-Type": "application/json; charset=utf-8"},
-                       timeout=self._TIMEOUT) as session:
+            timeout = aiohttp.ClientTimeout(self._TIMEOUT)
+            ) as session:
             tasks = []
             for block in block_query:
                 tasks.append(self._send_request(session, block))
-                if len(tasks) > limit:
+                if len(tasks) > int(limit * 0.9):
                     await asyncio.sleep(60)
             
             results = await asyncio.gather(*tasks)
+        
+        return results
 
+    def score(self, seqs):
+        """Asynchronously calcualtes the complexity score for a given sequence from the IDT API."""
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        try:
+            results = loop.run_until_complete(self.scoring(seqs))
+        finally:
+            loop.close()       
         penalties = self.parse_score(results)
         idt_complexity_score = [penalty * self.weight for penalty in penalties]
 
