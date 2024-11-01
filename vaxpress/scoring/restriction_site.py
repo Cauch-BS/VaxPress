c06301cfa93edce7d9fbd83d59d95bdbe6dafe1b
Chaebeom Sheen <cauchybs@snu.ac.kr>
2024-11-01 14:16:53 +0900

    debug: set default restriction weight as 0
    added sensible defaults for restriction sites
    avoided initialization in the absence of an argument
    added "original count" to reflect how many resites are in the original
    seq

--- vaxpress/scoring/restriction_site.py
+++ vaxpress/scoring/restriction_site.py
@@ -1,7 +1,30 @@
-# Example code that optimizes a sequence to have a specific restriction enzyme
-# recognition site.
+#
+# VaxPress
+#
+# Copyright 2023 Seoul National University
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# “Software”), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+# NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+# THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
 
-from vaxpress.scoring import ScoringFunction
+
+from . import ScoringFunction
 
 
 class RestrictionSiteFitness(ScoringFunction):
@@ -16,7 +39,7 @@ class RestrictionSiteFitness(ScoringFunction):
             "weight",
             dict(
                 type=float,
-                default=-100.0,  # penalize for the violations
+                default=0.0,  # penalize for the violations
                 help="scoring weight for the presence of the restriction site "
                 "(default: -100.0)",
             ),
@@ -30,15 +53,26 @@ class RestrictionSiteFitness(ScoringFunction):
                 "U (default: None)",
             ),
         ),
+        (
+            "original-sites",
+            dict(
+                default=0.0,
+                type=str,
+                help="number of restriction sites in the original sequence ",
+            ),
+        ),
     ]
 
     # Argument "_length_cds" is always passed to the constructor. Leave it as
     # it is even if you don't use it.
-    def __init__(self, weight, restriction_sites, _length_cds):
+    def __init__(self, weight, restriction_sites, original_sites, _length_cds):
         self.weight = weight
-        self.restriction_sites = restriction_sites.split(",")
-        if self.restriction_sites is None:
-            raise EOFError  # disable this scoring function
+        self.restriction_sites = (
+            restriction_sites.split(",") if restriction_sites else []
+        )
+        self.original_sites = original_sites
+        if not self.restriction_sites:
+            self.weight = 0.0
 
         for i, site in enumerate(self.restriction_sites):
             if not site:
@@ -49,13 +83,12 @@ class RestrictionSiteFitness(ScoringFunction):
             self.restriction_sites[i] = new_site
 
     def score(self, seqs):
-        has_site = []
+        metrics = []
         scores = []
         for seq in seqs:
-            found = -len(self.restriction_sites)
+            found = -self.original_sites
             for site in self.restriction_sites:
-                if site in seq:
-                    found += 1
-            has_site.append(found)
+                found += seq.count(site)
+            metrics.append(found)
             scores.append(found * self.weight)
-        return {"resite": scores}, {"resite": has_site}
+        return {"resite": scores}, {"resite": metrics}
